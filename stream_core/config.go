f765021f3f564b6bd83478272e1bfbcfa157ed65
vaziria <manorder123@gmail.com>
2026-01-03 17:15:44 +0700

    remove unused configuration

--- stream_core/config.go
+++ stream_core/config.go
@@ -2,13 +2,11 @@ package stream_core
 
 import (
 	"os"
-
-	"github.com/wargasipil/stream_engine/proto_core/wal_message/v1"
 )
 
 type CoreConfig struct {
-	WalDir           string
-	WalSerialization wal_message.WalSerialization
+	// WalDir           string
+	// WalSerialization wal_message.WalSerialization
 
 	HashMapCounterPath string
 	// must n^2 for the size
@@ -18,7 +16,7 @@ type CoreConfig struct {
 
 func NewDefaultCoreConfig() *CoreConfig {
 	return &CoreConfig{
-		WalDir:              "/tmp/stream_engine/wal",
+		// WalDir:              "/tmp/stream_engine/wal",
 		HashMapCounterPath:  "/tmp/stream_engine/hm_counter",
 		HashMapCounterSlots: 536_870_912,
 		DynamicValuePath:    "/tmp/stream_engine/dynamic_value",
@@ -27,7 +25,7 @@ func NewDefaultCoreConfig() *CoreConfig {
 
 func NewDefaultCoreConfigTest() *CoreConfig {
 	return &CoreConfig{
-		WalDir:              "/tmp/stream_engine/wal_test",
+		// WalDir:              "/tmp/stream_engine/wal_test",
 		HashMapCounterPath:  "/tmp/stream_engine/hm_counter_test",
 		HashMapCounterSlots: 32,
 		DynamicValuePath:    "/tmp/stream_engine/dynamic_value_test",
